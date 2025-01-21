012c8205987632f9e05eb00e345972183f5167fe
Jesse Kunze <kunze.jesse@gmail.com>
2025-01-20 17:28:39 -0800

    Git workflow and stack

--- lib/angus-cdk-stack.ts
+++ lib/angus-cdk-stack.ts
@@ -1,16 +1,38 @@
-import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AngusCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class AngusCdkStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // S3 bucket to host the website content
+        const bucket = new s3.Bucket(this, 'WebsiteBucket', {
+            // Block all public access to the bucket
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AngusCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // CloudFront distribution for secure public access
+        const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+            defaultBehavior: {
+                origin: new cloudfrontOrigins.S3Origin(bucket),
+            },
+        });
+
+        // Deploy the website build to the S3 bucket
+        new s3Deployment.BucketDeployment(this, 'DeployWebsite', {
+            sources: [s3Deployment.Source.asset('../angus-demo/dist')],
+            destinationBucket: bucket,
+            distribution, // Invalidate the cache on deployment
+            distributionPaths: ['/*'], // Clear all paths
+        });
+
+        // Output the CloudFront distribution URL
+        new CfnOutput(this, 'WebsiteURL', {
+            value: distribution.distributionDomainName,
+        });
+    }
 }
