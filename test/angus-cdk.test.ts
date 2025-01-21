0d5bfbfd6dd8501411896a81ae7958905f434e45
Jesse Kunze <kunze.jesse@gmail.com>
2025-01-20 16:54:31 -0800

    Initial commit

---
+++ test/angus-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AngusCdk from '../lib/angus-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/angus-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AngusCdk.AngusCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
