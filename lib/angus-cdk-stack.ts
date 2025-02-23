0851505cdbbd389ded57e5f5d1673fdf8bf9695b
Jesse Kunze <kunze.jesse@gmail.com>
2025-02-22 17:45:28 -0800

    Testing change to cfront root

--- lib/angus-cdk-stack.ts
+++ lib/angus-cdk-stack.ts
@@ -8,6 +8,7 @@ import { Construct } from 'constructs';
 export class AngusCdkStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+        const isCi = process.env.CI;
 
         // S3 bucket to host the website content
         const bucket = new s3.Bucket(this, 'WebsiteBucket', {
@@ -15,14 +16,15 @@ export class AngusCdkStack extends Stack {
             blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
         });
 
+        // const rootObject = isCi ? 'dist/index.html' : 'index.html';
         // CloudFront distribution for secure public access
         const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
-            defaultRootObject: 'index.html',
+            defaultRootObject: 'dist/index.html',
             defaultBehavior: {
                 origin: new cloudfrontOrigins.S3Origin(bucket),
             },
         });
-        const distPath = process.env.CI ? './dist' : '../angus-demo/dist';
+        const distPath = isCi ? './dist' : '../angus-demo/dist';
 
         // Deploy the website build to the S3 bucket
         new s3Deployment.BucketDeployment(this, 'DeployWebsite', {
